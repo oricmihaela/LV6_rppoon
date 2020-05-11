9b07f7d0f5fb7a6058bf2db43ff42b803dc379e2
Mihaela Orić <oricmihaela@gmail.com>
2020-05-11 16:10:40 +0200

    Add project files.

---
+++ LV6_rppoon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LV6_rppoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
